776200f874a033c5925e7ea666e4ebbe04678cc2
Ronald Cheng <ronaldcheng14@gmail.com>
2016-05-15 00:55:10 -0700

    testing of acc/gyro divider - not working

--- MPU9250.hpp
+++ MPU9250.hpp
@@ -28,10 +28,20 @@
 // Magnetometer (AK8963) Register Mapping
 // NOTE: RSV (0x0B), TS1 (0x0D), TS2 (0x0E) DO NOT ACCESS
 
+// Set initial input parameters
+enum Ascale {
+  AFS_2G = 0x00,
+  AFS_4G = 0x08,
+  AFS_8G = 0x10,
+  AFS_16G = 0x18
+};
 
-// Accelerometer & Gyroscope Register mapping
-#define GYRO_FULL_SCALE_2000_DPS    0x18
-#define ACC_FULL_SCALE_16_G         0x18
+enum Gscale {
+  GFS_250DPS = 0x00,
+  GFS_500DPS = 0x08,
+  GFS_1000DPS = 0x10,
+  GFS_2000DPS = 0x18
+};
 
 // << DEVICE ADDRESSES >>
 #define    AK8963_ADDRESS      0x0C    // Address of magnetometer
@@ -45,25 +55,81 @@ class MPU9250 : public scheduler_task
         {
         }
 
-        bool init(void)
+        void initAK8963(float )
         {
-            // Verify device identity
+
+        }
+
+        void verifyMPU9250(void)
+        {
             uint8_t WAI = mpu.readReg(MPU9250_ADDRESS, MPU9250_WAI);
             if(WAI == MPU9250_WhoAmIExpectedValue) {
                 u0_dbg_printf("\nI2C Read Test Passed, MPU6500 Address: 0x%x\n", WAI);
 
             } else {
                 u0_dbg_printf("ERROR: I2C MPU6500 Read Test Failed, Stopping\n");
-                while(1){}
+                while(1){}  // Infinite loop
             }
+        }
+
+        void setGyroScale(Gscale gyroFS)
+        {
+            mpu.writeReg(MPU9250_ADDRESS, GYRO_CONFIG, gyroFS);
+            switch (gyroFS)
+            {
+            // Possible gyro scales (and their register bit settings) are:
+            // 250 DPS (00), 500 DPS (01), 1000 DPS (10), and 2000 DPS  (11).
+                case GFS_250DPS:
+                    gyroDivider = 131;
+                    break;
+                case GFS_500DPS:
+                    gyroDivider = 65.5;
+                    break;
+                case GFS_1000DPS:
+                    gyroDivider = 32.8;
+                    break;
+                case GFS_2000DPS:
+                    gyroDivider = 16.4;
+                    break;
+            }
+        }
+
+        void setAccScale(Ascale accFS)
+        {
+            mpu.writeReg(MPU9250_ADDRESS, ACCEL_CONFIG, accFS);
+            switch (accFS){
+                case AFS_2G:
+                    accDivider = 16384;
+                    break;
+                case AFS_4G:
+                    accDivider = 8192;
+                    break;
+                case AFS_8G:
+                    accDivider = 4096;
+                    break;
+                case AFS_16G:
+                    accDivider = 2048;
+                    break;
+            }
+        }
+
+        bool init(void)
+        {
+            // Initialize MPU9250 device; wake up device
+            mpu.writeReg(MPU9250_ADDRESS, PWR_MGMT_1, 0x00);
+
+            verifyMPU9250(); // Verify device identity
 
             // Auto select clock source and Enable Acc & Gyro
-            mpu.writeReg(MPU9250_ADDRESS, PWR_MGMT_1, 0x01);
+            mpu.writeReg(MPU9250_ADDRESS, PWR_MGMT_1, 0x01);    // PLL with x-axis gyroscope reference
             mpu.writeReg(MPU9250_ADDRESS, PWR_MGMT_2, 0x00);
 
-            // Acc & Gyro sample configuration
-            mpu.writeReg(MPU9250_ADDRESS, GYRO_CONFIG, GYRO_FULL_SCALE_2000_DPS);
-            mpu.writeReg(MPU9250_ADDRESS, ACCEL_CONFIG, ACC_FULL_SCALE_16_G);
+            mpu.writeReg(MPU9250_ADDRESS, USER_CTRL, 0x20);     // I2C Master mode
+            mpu.writeReg(MPU9250_ADDRESS, MST_CTRL, 0x0D);      // 400 kHz ; 20 divider
+
+            // Set accelerometer & gyroscope sensitivity
+            setGyroScale(GFS_2000DPS);
+            setAccScale(AFS_16G);
 
             return true;
         }
@@ -81,23 +147,62 @@ class MPU9250 : public scheduler_task
             return ((MSB << 8) | (LSB & 0xFF));
         }
 
+        /*
+         * Each 16-bit accelerometer measurement has a full scale defined in ACCEL_FS
+         * (Register 28). For each full scale setting, the accelerometers' sensitivity
+         * per LSB in ACCEL_xOUT is shown in the table below:
+         *
+         * <pre>
+         * AFS_SEL | Full Scale Range | LSB Sensitivity
+         * --------+------------------+----------------
+         * 0       | +/- 2g           | 8192 LSB/mg
+         * 1       | +/- 4g           | 4096 LSB/mg
+         * 2       | +/- 8g           | 2048 LSB/mg
+         * 3       | +/- 16g          | 1024 LSB/mg
+         * </pre>
+         */
+        void getAcceleration(int16_t *rax, int16_t *ray, int16_t *raz)
+        {
+            *rax = get16BitRegister(ACCEL_XOUT_H) / accDivider;
+            *ray = get16BitRegister(ACCEL_YOUT_H) / accDivider;
+            *raz = get16BitRegister(ACCEL_YOUT_H) / accDivider;
+        }
+
+        /*
+         * Each 16-bit gyroscope measurement has a full scale defined in FS_SEL
+         * (Register 27). For each full scale setting, the gyroscopes' sensitivity per
+         * LSB in GYRO_xOUT is shown in the table below:
+         *
+         * <pre>
+         * FS_SEL | Full Scale Range   | LSB Sensitivity
+         * -------+--------------------+----------------
+         * 0      | +/- 250 degrees/s  | 131 LSB/deg/s
+         * 1      | +/- 500 degrees/s  | 65.5 LSB/deg/s
+         * 2      | +/- 1000 degrees/s | 32.8 LSB/deg/s
+         * 3      | +/- 2000 degrees/s | 16.4 LSB/deg/s
+         * </pre>
+         */
+        void getRotation(int16_t *rgx, int16_t *rgy, int16_t *rgz)
+        {
+            *rgx = get16BitRegister(GYRO_XOUT_H) / 16.4;
+            *rgy = get16BitRegister(GYRO_YOUT_H) / 16.4;
+            *rgz = get16BitRegister(GYRO_ZOUT_H) / 16.4;
+        }
+
         bool run(void *p)
         {
-            // Accelerometer
-            int16_t AcX = get16BitRegister(ACCEL_XOUT_H);
-            int16_t AcY = get16BitRegister(ACCEL_YOUT_H);
-            int16_t AcZ = get16BitRegister(ACCEL_ZOUT_H);
+            int16_t AcX, AcY, AcZ, GyX, GyY, GyZ;
 
-            // Gyproscope
-            int16_t GyX = get16BitRegister(GYRO_XOUT_H);
-            int16_t GyY = get16BitRegister(GYRO_YOUT_H);
-            int16_t GyZ = get16BitRegister(GYRO_ZOUT_H);
+            getAcceleration(&AcX, &AcY, &AcZ);       // Accelerometer
+            getRotation(&GyX, &GyY, &GyZ);           // Gyroscope
 
             double roll = atan2(AcY, AcZ) * 57.2957;
             double pitch = atan2(double(-AcX), sqrt(double(AcY * AcY) + double(AcZ * AcZ)));
 
             printf("roll: %f pitch %f\n", roll, pitch);
-            vTaskDelay(1000);
+            printf("gyro %f %f %f\n", GyX, GyY, GyZ);
+
+            vTaskDelay(2000);
 
 
 
@@ -107,15 +212,21 @@ class MPU9250 : public scheduler_task
     private:
         I2C2 &mpu = I2C2::getInstance();        // Get I2C bus semaphore
 
+        float accDivider = 0;
+        float gyroDivider = 0;
+
         /// Expected value of Sensor's "WHO AM I" register
         static const unsigned char MPU9250_WhoAmIExpectedValue = 0x71;
         typedef enum {
+            MST_CTRL=0x24, 
+            I2C_SLV0_ADDR=0x25, I2C_SLV0_REG=0x26, I2C_SLV0_CTRL=0x27,
+
             GYRO_CONFIG=0x1B, ACCEL_CONFIG=0x1C,
 
             ACCEL_XOUT_H=0x3B, ACCEL_YOUT_H=0x3D, ACCEL_ZOUT_H=0x3F,
             GYRO_XOUT_H=0x43, GYRO_YOUT_H=0x45, GYRO_ZOUT_H=0x47,
 
-            PWR_MGMT_1=0x6B, PWR_MGMT_2=0x6C,
+            USER_CTRL=0x6A, PWR_MGMT_1=0x6B, PWR_MGMT_2=0x6C,
             MPU9250_WAI=0x75,
 
         } __attribute__ ((packed)) MPU9250_RegisterMap;
